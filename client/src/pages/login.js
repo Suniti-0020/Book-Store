75c2c54c9ee806c173f5861e2fa393d3a6efbe26
Suniti-0020 <119940275+Suniti-0020@users.noreply.github.com>
2023-07-26 22:35:52 +0530

    login an register page design

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -15,13 +15,22 @@ function Login(props) {
         <div>
             <div className="auth-form-container">
                 <form className="login-form form" onSubmit={handleSubmit}>
-                    <h2>Login</h2>
-                    <label htmlFor="email">Username</label>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                    <label htmlFor="password">password</label>
-                    <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                    <button className="btn" type="submit">Log In</button>
-                    <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+                    <div className="separate">
+                        <img className="auth-img" src="https://www.globalsign.com/application/files/4616/2199/5695/ManagedPKI_Strong_Authentication_Cover_1_APAC_2021_05_19.jpg" alt="authentication " />
+                    </div>
+                    <div>
+                        <p className="change">
+                            Don't have an account <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Register</button>
+                        </p>
+                        <h3>Weclome to BooKraze</h3>
+                        <p>Dicover a world of stories </p>
+                        <h1>Login</h1>
+                        <label className="label" htmlFor="email">Username</label>
+                        <input className="input" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                        <label className="label" htmlFor="password">Password</label>
+                        <input className="input" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                        <button className="btn" type="submit">Log In</button>
+                    </div>
                 </form>
             </div>
         </div>
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -16,15 +16,23 @@ function Register(props) {
 
             <div className="auth-form-container register">
                 <form className="register-form form" onSubmit={handleSubmit}>
-                    <h2>Register</h2>
-                    <label htmlFor="name">Full name</label>
-                    <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
-                    <label htmlFor="email">email</label>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                    <label htmlFor="password">password</label>
-                    <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                    <button className="btn" type="submit">Log In</button>
-                    <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+                    <div className="separate">
+                        <img className="auth-img" src="https://www.globalsign.com/application/files/4616/2199/5695/ManagedPKI_Strong_Authentication_Cover_1_APAC_2021_05_19.jpg" alt="authentication " />
+                    </div>
+                    <div>
+                        <p className="change">Already have an account
+                            <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Login </button></p>
+                        <h3>Weclome to BooKraze</h3>
+                        <p>Dicover a world of stories </p>
+                        <h1>Register</h1>
+                        <label className="label" htmlFor="name">Full name</label>
+                        <input className="input" value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
+                        <label className="label" htmlFor="email">E-mail</label>
+                        <input className="input" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                        <label className="label" htmlFor="password">Password</label>
+                        <input className="input" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                        <button className="btn" type="submit">Register</button>
+                    </div>
                 </form>
             </div>
 

7571c408bc929777d098c2a6e588bf806c1c6164
Sarita <121181405+Sarita-021@users.noreply.github.com>
2023-07-27 20:01:42 +0530

    All-book page ui redesign

--- client/src/pages/button.js
+++ client/src/pages/button.js
@@ -5,11 +5,11 @@ import bookspedia from "../bookspedia";
 const Buttons = ({ filterItem, setItem, menuItems }) => {
     return (
         <>
-            <div className="d-flex justify-content-center">
+            <div className="d-flex filterBtn">
                 {menuItems.map((Val, id) => {
                     return (
                         <button
-                            className="btn-dark text-white p-1 px-2 mx-5 btn fw-bold"
+                            className="btn "
                             onClick={() => filterItem(Val)}
                             key={id}
                         >
@@ -18,7 +18,7 @@ const Buttons = ({ filterItem, setItem, menuItems }) => {
                     );
                 })}
                 <button
-                    className="btn-dark text-white p-1 px-3 mx-5 fw-bold btn"
+                    className="btn"
                     onClick={() => setItem(bookspedia)}
                 >
                     All

2e87ecb9a6dd465a690082d1e320850a6fdd14b0
Sarita <121181405+Sarita-021@users.noreply.github.com>
2023-07-29 23:00:27 +0530

    bug 2 resolve

--- client/src/index.js
+++ client/src/index.js
@@ -6,16 +6,9 @@ import "../src/CSS/index.css"
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from "../src/pages/redux/store";
-import { Provider } from 'react-redux';
-import { store } from "../src/pages/redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </Provider>
     <Provider store={store}>
         <React.StrictMode>
             <App />

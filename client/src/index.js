d981a192a0a89993bf0814bfb6dd7bae296b712c
Suniti-0020 <119940275+Suniti-0020@users.noreply.github.com>
2023-07-29 17:30:25 +0530

    navbar design

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './CSS/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';

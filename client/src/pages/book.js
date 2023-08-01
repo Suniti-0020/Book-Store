9cdbf346e091c94bfc766798d4230fa87f41fd27
Suniti-0020 <119940275+Suniti-0020@users.noreply.github.com>
2023-08-01 22:54:48 +0530

    bug fix order now

--- client/src/pages/book.js
+++ client/src/pages/book.js
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import Button from '@mui/material/Button';
-import TurnedInNotIcon from '@mui/icons-material/TurnedInNot';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
 import "../images/.."
 import "../CSS/book.css"
 
@@ -18,7 +15,7 @@ const Book = ({ item }) => {
                 return (
                     <div className="container" data-aos="zoom-in-up" >
                         <div class="box-image">
-                            <img src={Val.image} width={150} height={150}  alt="..." />
+                            <img src={Val.image} width={150} height={190} alt="..." />
                         </div>
                         <div class="box-text">
                             <h1 className="name">{Val.name}</h1>

287815e68e2ab6d9396c16aac1d19f98e23af70a
Suniti-0020 <119940275+Suniti-0020@users.noreply.github.com>
2023-08-29 21:34:05 +0530

    Added Search functionality in all books section

--- client/src/pages/allbooks.js
+++ client/src/pages/allbooks.js
@@ -24,16 +24,61 @@ function AllBooks() {
 
 
 
+    const [searchField, setSearchField] = useState("");
+    const [searchShow, setSearchShow] = useState(false);
+
+
+    const filteredBooks = bookspedia.filter(
+        items => {
+            return (
+                items
+                    .name
+                    .toLowerCase()
+                    .includes(searchField.toLowerCase())
+            );
+
+        }
+    );
+
+    const handleChange = e => {
+        setSearchField(e.target.value);
+        if (e.target.value === "") {
+            setSearchShow(false);
+        }
+        else {
+            setSearchShow(true);
+        }
+    };
+
+    const filtered = filteredBooks.map((item) => item.name);
+
+
     return (
         <div id="popular-container">
-            <div className="featured">
-                <h1 className="aBH1">All Books</h1>
-                <Buttons
-                    filterItem={filterItem}
-                    setItem={setItem}
-                    menuItems={menuItems}
-                />
-                <Book item={item} />
+            <div className="">
+                <div className="srch">
+                    <div className="tc ma0 pa4">
+                        <div className="pa2">
+                            <input
+                                className="pa3 bb br3 grow b--none bg-lightest-blue ma3"
+                                type="search"
+                                placeholder="Search book"
+                                onChange={handleChange}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="featured">
+                    <h1 className="aBH1">All Books</h1>
+
+                    <Buttons
+                        filterItem={filterItem}
+                        setItem={setItem}
+                        menuItems={menuItems}
+                    />
+                    <Book item={searchShow ? filteredBooks : item} />
+
+                </div>
             </div>
         </div>
     )
